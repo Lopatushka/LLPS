bc0f4b381dfa0a4b9ab2b6fcaa4f456683115521
Elena Lopatukhina <helena.lopatukhina@gmail.com>
2026-02-27 15:45:52 +0100

    ROIs im main()

--- FRAP.py
+++ FRAP.py
@@ -1,14 +1,12 @@
-from ij import IJ, WindowManager
-from ij.plugin import ChannelSplitter
+from ij import IJ
 from ij.plugin.frame import RoiManager
 from ij.measure import ResultsTable
-from ij.gui import GenericDialog
 from ij.process import ImageStatistics
 from ij.measure import Measurements
 import os
 
 def get_rm():
-    rm = RoiManager.getInstance()
+    rm = RoiManager.getInstance() 
     if rm is None:
         rm = RoiManager()
     return rm
@@ -20,19 +18,12 @@ def close_image(imp):
     imp.changes = False
     imp.close()
 
-def measure_rois(imp):
+def measure_rois(imp, rois):
     """
     single_ch_imp : ImagePlus (1-channel, may still be Z/T)
     rois          : list of Roi
     out_csv_path  : str
-    """
-    # Run ROI manager
-    rm = get_rm()
-    rois = rm.getRoisAsArray()
-    if rois is None or len(rois) == 0:
-        IJ.error("No ROIs in ROI Manager.")
-        return
-    
+    """    
     nT = imp.getNFrames()
     #nZ = single_ch_imp.getNSlices()
     rt = ResultsTable()
@@ -73,18 +64,32 @@ def main():
     img_title = imp.getTitle()
     safe_title = safe_name(img_title)
 
+    # Run ROI manager
+    rm = get_rm()
+    rois = rm.getRoisAsArray()
+    if rois is None or len(rois) == 0:
+        IJ.error("No ROIs in ROI Manager.")
+        return
+
     # Ask which channel is your fluorophore + where to save
     output_dir = IJ.getDirectory("Choose a directory to save data")
     if output_dir is None:
         IJ.error("No output directory selected!")
         return
 
-    # Measure over time and save CSV
-    data = measure_rois(imp)
+    # Measure Mean internsity over time for the ROIs in ROI manager
+    data = measure_rois(imp, rois)
+
+    # Save results in .CSV file
     data_path = os.path.join(output_dir, safe_title + ".csv")
     data.save(data_path)
 
-    IJ.log("Saved ROI mean intensities to: " + output_dir)
+    # Save ROIs
+    zip_path = os.path.join(output_dir, safe_title + "_ROIs.zip")
+    rm.runCommand("Save", zip_path)
+    rm.reset() # clean ROI manager
+
+    IJ.log("Saved ROI mean intensities and ROIs.zip to: " + output_dir)
     close_image(imp)
 
 main()
