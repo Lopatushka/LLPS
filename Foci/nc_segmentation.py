96ae1ca821fb7e05b99f4572af1267769976c24a
Lopatushka <helena.lopatukhina@gmail.com>
2026-06-15 18:05:21 +0200

    del fun draft

--- Foci/nc_segmentation.py
+++ Foci/nc_segmentation.py
@@ -138,6 +138,24 @@ def subtract_background(imp, radius, light_background=False, use_paraboloid=Fals
     )
     imp.updateAndDraw()
     
+
+def ask_user_to_draw_roi(title, message, roi_name, rm):
+    IJ.setTool("freehand")
+
+    WaitForUserDialog(title, message).show()
+
+    roi = IJ.getImage().getRoi()
+
+    if roi is None:
+        IJ.showMessage("Error", "No ROI was drawn.")
+        return None
+
+    rm.addRoi(roi)
+    rm.select(rm.getCount() - 1)
+    rm.rename(roi_name)
+
+    return roi
+    
 def image_processing(imp, output_dir, p):
     '''
     This function process semi-manually a single image
@@ -197,79 +215,6 @@ def cleanup_iteration():
         rm.reset()
         rm.close()
 
-def draw_nuclei(rm):
-    IJ.setTool("freehand")
-
-    WaitForUserDialog(
-        "Nuclei Segmentation",
-        "Draw nuclei ROIs.\n"
-        "Press 't' after each ROI to add it to ROI Manager.\n"
-        "Click OK when finished."
-    ).show()
-
-    n_nuclei = rm.getCount()
-
-    for i in range(n_nuclei):
-        rm.select(i)
-        rm.rename("Nucleus_%d" % (i + 1))
-
-    return n_nuclei
-
-def draw_cells(rm, n_nuclei):
-    WaitForUserDialog(
-        "Cell Segmentation",
-        "Draw whole-cell ROIs.\n"
-        "Press 't' after each ROI.\n"
-        "Click OK when finished."
-    ).show()
-
-    total_rois = rm.getCount()
-    n_cells = total_rois - n_nuclei
-
-    for i in range(n_cells):
-        rm.select(n_nuclei + i)
-        rm.rename("Cell_%d" % (i + 1))
-
-    return n_cells
-
-def create_mask(width, height, title):
-    return IJ.createImage(title, "8-bit black", width, height, 1)
-
-
-def fill_mask(mask, rois):
-    for roi in rois:
-        mask.setRoi(roi)
-        IJ.run(mask, "Fill", "slice")
-        
-def build_masks(rm, n_nuclei, n_cells, width, height):
-    nuclei_mask = create_mask(width, height, "Nuclei_mask")
-    cell_mask = create_mask(width, height, "Cell_mask")
-
-    nuclei_rois = [rm.getRoi(i) for i in range(n_nuclei)]
-    cell_rois = [rm.getRoi(n_nuclei + i) for i in range(n_cells)]
-
-    fill_mask(nuclei_mask, nuclei_rois)
-    fill_mask(cell_mask, cell_rois)
-
-    return nuclei_mask, cell_mask
-
-
-def create_cytoplasm_mask(cell_mask, nuclei_mask):
-    cyto_mask = cell_mask.duplicate()
-    cyto_mask.setTitle("Cytoplasm_mask")
-
-    IJ.run(
-        cyto_mask,
-        "Image Calculator...",
-        "image1=Cytoplasm_mask operation=Subtract image2=Nuclei_mask create"
-    )
-
-    result = WindowManager.getImage("Result of Cytoplasm_mask")
-
-    if result:
-        result.setTitle("Cytoplasm_mask")
-
-    return result
 
 def main():
     # Check if at least one image is opened
